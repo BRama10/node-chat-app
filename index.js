22d67d261e0c714ae88244ac4974fc4a0ee9f34c
BRama10 <grand.hunter.dark.15@gmail.com>
2023-03-13 09:39:04 -0400

    Saving Files

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ const app = express();
 var server = http.Server(app);
 
 var io = require('socket.io')(server);
+
+const PORT_NUMBER = 3001;
+
+
+server.listen(PORT_NUMBER, function() {
+    console.log('Server Started!');
+})
+
+io.on('connection', function(socket) {
+    //socket.emit('new_user');
+    console.log('New User');
+})
